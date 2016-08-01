6b4f5d2105a768c2b5b10a81350f1f91cf7fb20c
vu.quang.duyet <quangduyet.vu@gmail.com>
2016-08-01 20:52:50 +0700

    The first commit

---
+++ DemoStyleLayout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoStyleLayout.Startup))]
+namespace DemoStyleLayout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
